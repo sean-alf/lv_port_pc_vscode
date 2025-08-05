a0dc8ba4da383b48143f6f5a57e230b2fc58b94b
Sean Alf <no-email>
2025-08-05 10:32:36 -0700

    screen brightness settings page

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "auto-door-ui-app/src/app_ui.h"
 
 #include "auto-door-ui-app/src/events.h"
+#include "auto-door-ui-app/src/pages/screen_brightness_page.h"
 #include "lvgl.h"
 
 #ifdef _MSC_VER
@@ -13,7 +14,16 @@
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
 
+#define SCREEN_BRIGHTNESS_MIN 1
+#define SCREEN_BRIGHTNESS_MAX 15
+#define SCREEN_BRIGHTNESS_STEP 1
+#define SCREEN_BRIGHTNESS_INIT 7
+
 uint32_t INFO_EVENT = 0;
+
+static void on_brightness_change(uint8_t val) {
+    LV_LOG_USER("brightness: %u", val);
+}
 
 int main(int argc, char** argv) {
     lv_init();
@@ -21,6 +31,15 @@ int main(int argc, char** argv) {
     INFO_EVENT = lv_event_register_id();
 
     app_ui_init(false, SCREEN_WIDTH, SCREEN_HEIGHT, NULL);
+
+    screen_brightness_page_config_t cfg = {};
+    cfg.initial_value = SCREEN_BRIGHTNESS_INIT;
+    cfg.min_value = SCREEN_BRIGHTNESS_MIN;
+    cfg.max_value = SCREEN_BRIGHTNESS_MAX;
+    cfg.step = SCREEN_BRIGHTNESS_STEP;
+    cfg.on_value_change_cb = on_brightness_change;
+
+    screen_brightness_page_init(cfg);
 
     while (1) {
         /* Periodically call the lv_task handler.
